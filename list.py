4555bb99a6cc0ef9b1a15b6b9395ca3aa6c7e2d9
Amal Anwar <56563932+amal4690@users.noreply.github.com>
2021-03-02 14:48:39 -0800

    Create list.py

---
+++ list.py
@@ -0,0 +1,22 @@
+l = ["a", "b","cdef"]
+print("".join(l)) 
+
+#prints abcdef as the output
+
+l = ["a", "b","cdef"]
+print(','.join(l)) 
+
+#prints a,b,cdef as the output
+
+l = ["a", "b","cdef"]
+for x in range(len(l)): #using for loop
+    print (l[x])
+    
+"""
+prints 
+a
+b
+cdef
+as the output
+"""
+
