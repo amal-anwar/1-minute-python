1d04730d5c8e46ae8974f5e8ad00030970ad9cd6
Amal Anwar <56563932+amal4690@users.noreply.github.com>
2021-03-06 16:18:29 -0800

    Create string-to-int.py

---
+++ string-to-int.py
@@ -0,0 +1,15 @@
+a = '2'
+b = '3'
+  
+# convert a using int 
+a = int(a) 
+  
+# convert b using int 
+b = int(b) 
+  
+# sum both integers 
+sum = a + b 
+  
+# as strings and integers can't be added 
+# test the sum 
+print(sum) 
