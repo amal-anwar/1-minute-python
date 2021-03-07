bad24848cec9301412caf8ea7a3c967ee9e737d1
Amal Anwar <56563932+amal4690@users.noreply.github.com>
2021-03-07 04:48:44 -0800

    Create string-to-list.py

---
+++ string-to-list.py
@@ -0,0 +1,16 @@
+s = 'hello'
+l = list(s)
+print(l)
+
+#will give the output
+#['h', 'e', 'l', 'l', 'o']
+#and
+
+s = 'hello'
+l = list(s)
+l[0] = 'H'
+l[1] = 'D'
+print(l)
+
+#will give the output
+#['H', 'D', 'l', 'l', 'o']
