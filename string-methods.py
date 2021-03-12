3525cbeb1989a5e34799ce668f95856d0ef861ec
Amal Anwar <56563932+amal4690@users.noreply.github.com>
2021-03-12 13:45:06 -0800

    Create string-methods.py

---
+++ string-methods.py
@@ -0,0 +1,38 @@
+#UPPERCASE
+h = 'hi'
+
+'''
+in the terminal 
+type -
+h.upper()
+output = 'HI'
+'''
+#LOWERCASE
+
+'''
+in the terminal 
+type -
+h.lower
+output = 'hi'
+'''
+#REPLACE
+
+'''
+in the terminal 
+type -
+h.replace ("hi","hello")
+output = 'hello'
+'''
+
+#COUNT
+
+h = 'Hi Amal'
+
+'''
+in the terminal 
+type -
+h.count ("a")
+output = 1
+h.count ("A")
+output = 1
+'''
