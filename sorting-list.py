fefd1ee14daddd08ed807522899c9b6d619c56e6
Amal Anwar <56563932+amal4690@users.noreply.github.com>
2021-03-11 16:54:19 -0800

    Create sorting-list.py

---
+++ sorting-list.py
@@ -0,0 +1,13 @@
+l= [3, -2, 11, 0, 5]
+
+"""
+>>> sorted(l) 
+[-2, 0, 3, 5, 11]
+>>> l
+[3, -2, 11, 0, 5]
+>>> l.sort
+<built-in method sort of list object at 0x000001DEAEBBC280>
+>>> l
+[3, -2, 11, 0, 5]
+
+"""
