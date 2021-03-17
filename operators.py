85b73dacaff45a43ab29b3c907007ea932d222d2
Amal Anwar <56563932+amal4690@users.noreply.github.com>
2021-03-17 05:07:14 -0700

    Create operators.py

---
+++ operators.py
@@ -0,0 +1,8 @@
+#operators
+
+print(3+2)     #addition
+print(3-2)     #subtraction
+print(3*2)     #multiplication
+print(3/2)     #division (quotient)
+print(3%2)     #division (remainder)
+print(3**2)    #3 to the power of 2
