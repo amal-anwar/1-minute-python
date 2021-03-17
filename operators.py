74553d7804b5ae8f211a2e722c9662ce1570f2a6
Amal Anwar <56563932+amal4690@users.noreply.github.com>
2021-03-17 05:16:56 -0700

    Update operators.py

--- operators.py
+++ operators.py
@@ -6,3 +6,15 @@ print(3*2)     #multiplication
 print(3/2)     #division (quotient)
 print(3%2)     #division (remainder)
 print(3**2)    #3 to the power of 2
+
+
+a = 2
+a+=1
+print(a)
+
+#same as
+
+b = 4
+b = b + 1
+print (b)
+
