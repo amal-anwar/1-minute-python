fb1d45d0b1c5af77ad7d1341263ec4e4534c7fea
Amal Anwar <56563932+amal4690@users.noreply.github.com>
2021-03-19 11:28:29 -0700

    Update difference-between-dates.py

--- difference-between-dates.py
+++ difference-between-dates.py
@@ -1,3 +1,13 @@
+#a simple program 
+from datetime import date
+f_date = date(2014, 7, 2)
+l_date = date(2014, 7, 11)
+delta = l_date - f_date
+print(delta.days)
+'''
+will give the output 9
+'''
+#program to find days till christmas 
 from datetime import date
 today = date.today ()
 christmas = date(date.today().year, 12,25)
