2d456e4e3d174deedb52ec129c64f9455ee74de7
Amal Anwar <56563932+amal4690@users.noreply.github.com>
2021-03-18 17:27:39 -0700

    Create backwards-string.py

---
+++ backwards-string.py
@@ -0,0 +1,27 @@
+'''
+A simple program
+a = "hello" [::-1]
+
+
+A complex program
+def my_function(x): #create a function that takes a strin
+  return x[::-1] #return the backward string
+
+mytxt = my_function("I wonder how this text looks like backwards")
+#calling the function with string as a parameter
+print(mytxt)
+'''
+#to run them together 
+def my_function(x): #create a function that takes a strin
+  return x[::-1] #return the backward string
+a = "hello" [::-1]
+mytxt = my_function("I wonder how this text looks like backwards")
+#calling the function with string as a parameter
+print(mytxt)
+print(a)
+
+'''
+will give the output
+sdrawkcab ekil skool txet siht woh rednow I
+olleh
+'''
