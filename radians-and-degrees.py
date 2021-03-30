2dbd3744c6ed24d7b4a8eaf948e8e464c61b03ee
Amal Anwar <56563932+amal4690@users.noreply.github.com>
2021-03-30 01:47:00 -0700

    Update radians-and-degrees.py

--- radians-and-degrees.py
+++ radians-and-degrees.py
@@ -1,11 +1,11 @@
 >>> import math
->>> math.degrees(math.pi)
+>>> math.degrees(math.pi) #math.pi = 3.14; here we get the value of PI radians in degrees
 180.0
->>> math.degrees(math.pi/2)
+>>> math.degrees(math.pi/2) #math.pi = 3.14; here we get the value of PI/2 radians in degrees
 90.0
->>> math.radians(98)
+>>> math.radians(98) #to get the radian of 98
 1.710422666954443
->>> math.radians(180)
+>>> math.radians(180) #to get the radian of 180
 3.141592653589793
->>> math.radians(0)
+>>> math.radians(0) #to get the radian of 0
 0.0
