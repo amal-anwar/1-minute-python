9a40ba07791380f87d0558d36a7106fea0d7781f
Amal Anwar <56563932+amal4690@users.noreply.github.com>
2021-03-30 01:01:49 -0700

    Create radians-and-degrees.py

---
+++ radians-and-degrees.py
@@ -0,0 +1,11 @@
+>>> import math
+>>> math.degrees(math.pi)
+180.0
+>>> math.degrees(math.pi/2)
+90.0
+>>> math.radians(98)
+1.710422666954443
+>>> math.radians(180)
+3.141592653589793
+>>> math.radians(0)
+0.0
