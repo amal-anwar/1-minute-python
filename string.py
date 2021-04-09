413005c4bc89edcbaec69f29af77e0a1bab3f9d3
Amal Anwar <56563932+amal4690@users.noreply.github.com>
2021-04-09 12:02:30 -0700

    Create string.py

---
+++ string.py
@@ -0,0 +1,21 @@
+>>> str_cs = 'hello amal'
+>>> print (str_cs) #prints entire string
+hello amal
+>>> print (str_cs [0]) #prints first character of string
+h
+>>> print (str_cs [2:5]) #prints characters starting from 3rd to 5th
+llo
+>>> print (str_cs [2:]) #prints string starting from 3rd character
+llo amal
+>>> print (str_cs * 7) #prints string 7 times
+hello amalhello amalhello amalhello amalhello amalhello amalhello amal
+>>> print (str_cs + "TEST") #prints concatenated string
+hello amalTEST
+>>> str_cs [-1:] #prints last character of string
+'l'
+>>> str_cs[::-1] #prints string in reverse order
+'lama olleh'
+>>> str_cs[::2] #prints alternated characters of string
+'hloaa'
+>>> str_cs[::3] #prints alternated characters of string
+'hlal'
