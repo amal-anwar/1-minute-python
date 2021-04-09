3f46296db0e7ce6eb4a92660c9afa9369405372d
Amal Anwar <56563932+amal4690@users.noreply.github.com>
2021-04-09 11:51:15 -0700

    Create variables.py

---
+++ variables.py
@@ -0,0 +1,4 @@
+>>> studentid=100
+>>> studentName="amal"
+>>> print ("Hello, my name is %s, and my ID is %d" % (studentName, studentid)) #%s for string and %d for decimal
+Hello, my name is amal, and my ID is 100
