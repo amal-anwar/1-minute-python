e3eeca61b1b6a9b9c0a131d32ed606fa6aa87068
Amal Anwar <56563932+amal4690@users.noreply.github.com>
2021-04-10 05:49:12 -0700

    Create user-input.py

---
+++ user-input.py
@@ -0,0 +1,6 @@
+username = input("Enter username:")
+print("Username is: " + username)
+
+ui = float(input ("Enter weight of watermelon in KG: "))
+pounds = ui*2.204
+print ("Weight of watermelon in pounds is: ", pounds)
