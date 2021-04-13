693421b965405d275c3e310fe98c1bafcefe34f1
Amal Anwar <56563932+amal4690@users.noreply.github.com>
2021-04-13 04:11:31 -0700

    Update function.py

--- function.py
+++ function.py
@@ -4,3 +4,18 @@ def greet (name):
 >>> greet ('Amal')
 Hello,  Amal. Good morning.
 >>> 
+
+def add(x,y):
+    
+    sum = x + y
+    return sum
+    
+num1 = int(input("Enter a number: "))
+num2 = int(input("Enter another number: "))
+
+print("The sum is: ", add(num1,num2))
+
+Enter a number: 1
+Enter another number: 2
+The sum is:  3
+>>>
