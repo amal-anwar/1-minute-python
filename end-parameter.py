fff5e09e13dfdb044ca97b56a9f37b874bfa5cca
Amal Anwar <56563932+amal4690@users.noreply.github.com>
2021-04-16 12:30:25 -0700

    Create end-parameter.py

---
+++ end-parameter.py
@@ -0,0 +1,13 @@
+print('one', end = '')
+print('one', end = '')
+print('one', end = '')
+print('one', end = '')
+
+print('\none', end = ' ')
+print('one', end = ' ')
+print('one', end = ' ')
+print('one', end = ' ')
+
+#output will be 
+oneoneoneone
+one one one one 
