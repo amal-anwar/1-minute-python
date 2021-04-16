e6eeb91330f81e75b7ffeff34049e0d273961e70
Amal Anwar <56563932+amal4690@users.noreply.github.com>
2021-04-16 12:22:59 -0700

    Create escape-characters.py

---
+++ escape-characters.py
@@ -0,0 +1,12 @@
+txt = "Amal is \"not\" here" # \ stops an error from taking place in this sentence
+print(txt)
+
+print("This is\n a new line") #prints everything after \n in a new line
+
+print(r'Hi\xHello')#r is a raw string that stops an error for \x
+
+print("Hello\tamal")#will print hello amal
+
+print("Hello\bamal")#will print hellamal
+
+print("Hello\r amal")#will print amal
