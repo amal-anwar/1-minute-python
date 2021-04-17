54381b13fffb33a5320c728f6a5c096b32780740
Amal Anwar <56563932+amal4690@users.noreply.github.com>
2021-04-17 14:54:29 -0700

    Methods for print function
    Codes to print 
    one,'two',"three",four,\five\,
    	once 'I' caught a fish
    		'//alive\\'
    using just the print function

---
+++ print.py
@@ -0,0 +1,7 @@
+print('one,\'two\',"three",four,\\five\\,\n\tonce \'I\' caught a ' +
+      'fish\n\t\t\'//alive\\\\\'')
+
+
+>>one,'two',"three",four,\five\,
+	once 'I' caught a fish
+		'//alive\\'
