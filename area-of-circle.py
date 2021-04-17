f5ec5bef8015a1bfd191b5920b241d9237b34a81
Amal Anwar <56563932+amal4690@users.noreply.github.com>
2021-04-17 15:40:05 -0700

    added another way of calculation
    #another method of calculating the area of a circle

--- area-of-circle.py
+++ area-of-circle.py
@@ -2,3 +2,12 @@ import math
 pi = 3.14
 radius = 2.2
 area = pi*(radius**2)
+____________________
+#another method of calculating the area of a circle
+ui = float(input('Radius?\n'))
+area=3.14*ui**2
+print(area)
+#will give output
+Radius?
+2
+12.56
