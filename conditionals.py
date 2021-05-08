7e5320d3ad871d2cb12eb012157978952ebf72ae
Amal Anwar <56563932+amal4690@users.noreply.github.com>
2021-05-08 09:29:03 -0700

    added more conditions
    Adding options for less than or equal to, more than or equal to, equal to and not equal to

--- conditionals.py
+++ conditionals.py
@@ -4,3 +4,15 @@ y =6
 print('x ==',x,' y ==',y)
 print('checking less than with \'<\':',x < y)
 print('checking more than with \'>\':',x > y)
+____________________________________________________
+
+x =5
+y =6
+
+print('x ==',x,' y ==',y)
+print('checking less than with \'<\':',x < y)
+print('checking more than with \'>\':',x > y)
+print('checking equal to with \'==\':',x == y)
+print('checking not equal to with \'!=\':',x != y)
+print('checking less than or equal to with \'<=\':',x <= y)
+print('checking more than or equal to with \'>=\':',x >= y)
