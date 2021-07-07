66ce43d96a751eef874df62259954d0f0ad82f08
Amal Anwar <56563932+amal4690@users.noreply.github.com>
2021-07-07 00:50:12 -0700

    Code that asks user for an int between 1 and 5
    The user will enter an int, and a string will be printed. If any other number aside from 1-5 is mentioned, 'number not valid' will be printed.

---
+++ int-to-string.py
@@ -0,0 +1,13 @@
+ui_input = int(input("Enter an integer between 1 to 5 - "));
+if ui_input == 1:
+    print('one')
+elif ui_input == 2:
+    print('two')
+elif ui_input == 3:
+    print('three')
+elif ui_input == 4:
+    print('four')
+elif ui_input == 5:
+    print('five')
+else:
+    print ('number not valid')
