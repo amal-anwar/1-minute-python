57f02c43ecc7d030314d6181af2fa4648c51bcda
Amal Anwar <56563932+amal4690@users.noreply.github.com>
2021-07-07 00:56:10 -0700

    Added code that asks user for a string value
    between one and five, the program will print an int, else it will print out of range.

--- string-to-int.py
+++ string-to-int.py
@@ -13,3 +13,20 @@ sum = a + b
 # as strings and integers can't be added 
 # test the sum 
 print(sum) 
+
+_____________________________________________
+
+ui_input = input("Enter a string between one and five - ");
+ui_input = ui_input.lower ()
+if ui_input == 'one':
+    print(1)
+elif ui_input == 'two':
+    print(2)
+elif ui_input == 'three':
+    print(3)
+elif ui_input == 'four':
+    print(4)
+elif ui_input == 'five':
+    print(5)
+else:
+    print ('Out of range')
