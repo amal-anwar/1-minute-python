c3d1281592c705533717090615db11f18fda7df0
Amal Anwar <56563932+amal4690@users.noreply.github.com>
2021-07-12 23:42:30 -0700

    simple program to check length

---
+++ name-length.py
@@ -0,0 +1,6 @@
+name = input("Write your name - ")
+name_len = len(name)
+if name_len >5:
+    print('Your name has',name_len,'characters')
+else: 
+    print("I\'m not telling you the length of your name")
