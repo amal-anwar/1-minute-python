fd721110be0e974b119345e6113dd4bf72845418
Amal Anwar <56563932+amal4690@users.noreply.github.com>
2021-08-13 07:04:41 -0700

    running a program using def

---
+++ def.py
@@ -0,0 +1,10 @@
+def hello(): 
+    print('Howdy!')
+    print('Howdy!!!')
+    print('Hello there.')
+    
+hello()
+hello()
+hello()
+#Since this program calls hello() three times, the code in the hello() function is
+#executed three times.
